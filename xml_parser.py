802a6285d8eb511ed043d97ff9f17a496a198711
Karthik <iamkarthiksiddarth@outlook.com>
2025-02-23 14:04:54 +0000

    add more fields

--- xml_parser.py
+++ xml_parser.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 def get_patient_info(patient):
     name = patient.find('Name').get('val')
+    hospital_number = patient.find('PatientID').get('val')
     data_of_birth = patient.find('BirthDate').get('val')
     age = patient.find('Age').get('val')
     height = patient.find('Height').get('val')
@@ -10,15 +11,19 @@ def get_patient_info(patient):
     bsa = patient.find('Bsa').get('val')
     patient_id = patient.find('PatientID').get('val')
     return {
-        'name': name, 'data_of_birth': data_of_birth, 'age': age, 'height': height, 'weight': weight, 'bsa': bsa, 'patient_id': patient_id
+        'name': name, 'hospital_number': hospital_number, 'data_of_birth': data_of_birth, 'age': age, 'height': height, 'weight': weight, 'bsa': bsa, 'patient_id': patient_id
     }
 
 
 def get_scanner_info(scan):
     scan_date = scan.find('StudyDate').get('val')
-    institution_name = scan.find('InstitutionName').get('val')
+    requesting_physician = scan.find('RequestingPhysician').get('val')
+    history = scan.find('History').get('val')
+    request = scan.find('Request').get('val')
+    manufacturer = scan.find('Manufacturer').get('val')
+    manufacturer_model_name = scan.find('ManufacturerModelName').get('val')
     return {
-        'scan_date': scan_date, 'institution_name': institution_name
+        'scan_date': scan_date, 'requesting_physician': requesting_physician, 'history': history, 'request': request, 'manufacturer': manufacturer, 'manufacturer_model_name': manufacturer_model_name
     }
 
 table_style = """
@@ -87,15 +92,23 @@ def get_data(xml_data):
     patient_data = get_patient_info(patient)
     scan_data = get_scanner_info(scan)
 
+    print(scan_data, 'scan_data')
+
+    weight = int(patient_data['weight'])
+    height = int(patient_data['height'])
+    height_in_m = height/100 if height > 0 else 0
+    bmi = weight / height_in_m ** 2 if weight > 0 and height_in_m > 0 else 'N/A'
+    scanner_type = scan_data['manufacturer'] + ' ' + scan_data['manufacturer_model_name']
+
     data = [
         ("Name:", patient_data['name'], "Date of Scan:", scan_data['scan_date']),
-        ("Date of Birth:", patient_data['data_of_birth'], "Hospital Number:", scan_data['institution_name']),
+        ("Date of Birth:", patient_data['data_of_birth'], "Hospital Number:", patient_data['hospital_number']),
         ("Height (cm):", patient_data['height'], "Weight (kg):", patient_data['weight']),
-        ("BSA (m2):", patient_data['bsa'], "BMI (kg/m2):", ""),
-        ("Referring Consultant:", "", "", ""),
-        ("Patient History:", "", "", ""),
-        ("Indication:", "", "", ""),
-        ("Scanner type:", "", "Field strength (T):", ""),
+        ("BSA (m^2):", patient_data['bsa'], "BMI (kg/m^2):", bmi),
+        ("Referring Consultant:", scan_data['requesting_physician'], "", ""),
+        ("Patient History:", scan_data['history'], "", ""),
+        ("Indication:", scan_data['request'], "", ""),
+        ("Scanner type:", scanner_type, "Field strength (T):", ""),
         ("Sequences acquired:", "", "", ""),
         ("Contrast agent type:", "", "Contrast agent dose (mmol/kg):", ""),
     ]
