2ce84f5e085d77b80e26b06cb0cff64d93a88e83
Karthik <iamkarthiksiddarth@outlook.com>
2025-02-26 11:30:56 +0000

    add dir

--- parse_site/settings.py
+++ parse_site/settings.py
@@ -131,3 +131,6 @@ if not DEBUG:    # Tell Django to copy static assets into a path called `staticf
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
